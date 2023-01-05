b6665fdda75f8566862b324f43b90a1f8ab6bfcf
umair-rao <umairmuqeem@gmail.com>
2023-01-05 13:50:21 +0500

    Add css active list class

--- src/App.js
+++ src/App.js
@@ -1,18 +1,16 @@
 import React from 'react';
 import { Routes, Route } from 'react-router-dom';
+import Navbar from './Components/Navbar';
 
-// pages
+import './App.css';
 import Rockets from './Pages/Rockets';
 import Missions from './Pages/Missions';
 import Dragons from './Pages/Dragons';
 import Profile from './Pages/Profile';
-import Navbar from './Components/Navbar';
-
-import './App.css';
 
 function App() {
   return (
-    <>
+    <div className="App">
       <Navbar />
       <Routes>
         <Route path="/" element={<Rockets />} />
@@ -20,7 +18,7 @@ function App() {
         <Route path="/dragons" element={<Dragons />} />
         <Route path="/profile" element={<Profile />} />
       </Routes>
-    </>
+    </div>
   );
 }
 
--- src/Components/Navbar.js
+++ src/Components/Navbar.js
@@ -1,33 +1,25 @@
 import React from 'react';
-import { NavLink, Link } from 'react-router-dom';
+import { Link, useLocation } from 'react-router-dom';
 import logo from '../logo/planet.png';
 import './Navbar.css';
 
-const Navbar = () => (
-  <nav className="navbar">
-    <div className="logo">
-      <img className="logo-image" src={logo} alt="Logo" />
-      <h1>Space Travelers` Hub</h1>
-    </div>
-
-    <div className="nav-links">
-      <ul className="menu-list">
-        <li className="list-item">
-          <Link to="/">Rockets</Link>
-        </li>
-        <li className="list-item">
-          <Link to="/missions">Missions</Link>
-        </li>
-        <li className="list-item">
-          <Link to="/dragons">Dragons</Link>
-        </li>
-        <li className="list-item">
-          <Link to="/profile">Profile</Link>
-        </li>
-      </ul>
-    </div>
-    <NavLink />
-  </nav>
-);
-
-export default Navbar;
+export default function Navbar() {
+  const location = useLocation();
+  const { pathname } = location;
+  return (
+    <nav className="navbar">
+      <Link to="/" className="logo">
+        <img className="logo-image" src={logo} alt="Logo" />
+        <h1>Space Travelers` Hub</h1>
+      </Link>
+      <div className="nav-links">
+        <ul>
+          <li><Link to="/" className={`link ${pathname === '/' ? 'active' : ''}`}>Rockets</Link></li>
+          <li><Link to="/missions" className={`link ${pathname === '/missions' ? 'active' : ''}`}>Missions</Link></li>
+          <li><Link to="/dragons" className={`link ${pathname === '/dragons' ? 'active' : ''}`}>Dragons</Link></li>
+          <li><Link to="/profile" className={`link ${pathname === '/profile' ? 'active' : ''}`}>Profile</Link></li>
+        </ul>
+      </div>
+    </nav>
+  );
+}
