426f1aa5ee45d97266a2845bf85f897dcad1916c
peter <peterugwu64@gmail.com>
2023-01-05 19:41:46 +0100

    add mission joining and leaving

--- src/Pages/Missions.js
+++ src/Pages/Missions.js
@@ -1,11 +1,15 @@
 import React, { useEffect } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
-import { fetchMissions } from '../Redux/Missions/Missions';
+import { fetchMissions, toggleJoining } from '../Redux/Missions/Missions';
 import DisplayMission from '../Components/DisplayMissions';
 
 const Missions = () => {
   const dispatch = useDispatch();
   const missions = useSelector((state) => state.Missions);
+
+  const handleJoining = (id) => {
+    dispatch(toggleJoining(id));
+  };
 
   useEffect(() => {
     if (missions.length === 0) {
@@ -22,7 +26,14 @@ const Missions = () => {
         <h3 className="gridTitle">Status</h3>
         <h3 className="gridTitle">Missions with description and status</h3>
       </div>
-      {missions.map((mission) => <DisplayMission key={mission.id} mission={mission} />)}
+      {missions.map((mission) => (
+        <DisplayMission
+          key={mission.mission_id}
+          mission={mission}
+          handleJoining={handleJoining}
+        />
+      ))}
+      ;
     </div>
   );
 };
--- src/Redux/utils/index.js
+++ src/Redux/utils/index.js
@@ -2,3 +2,8 @@ export default function pick(obj, arr) {
   // eslint-disable-next-line
     return arr.reduce((acc, record) => (record in obj && (acc[record] = obj[record]), acc), {});
 }
+
+export const addJoin = (arr) => {
+  const missions = arr.map((obj) => ({ ...obj, joined: false }));
+  return missions;
+};
