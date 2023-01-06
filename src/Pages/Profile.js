59f436f3855437ee441a7655e64f3929b9c58746
peter <peterugwu64@gmail.com>
2023-01-06 11:39:54 +0100

    Filters mission on profile

--- src/Pages/Profile.js
+++ src/Pages/Profile.js
@@ -1,23 +1,33 @@
 import React from 'react';
+import { useSelector } from 'react-redux';
 
 import MissionLists from '../Components/MissionLists';
-import RocketLists from '../Components/RocketLists';
+// import RocketLists from '../Components/RocketLists';
 
 import './Profile.css';
 
-const Profile = () => (
-  <>
-    <div className="profile-container">
-      <div className="profile-missions-section">
-        <h3 className="mission-lists">My Missions</h3>
-        <MissionLists />
+const Profile = () => {
+  const missions = useSelector((state) => state.Missions);
+  const joinedMissions = missions.filter((mission) => mission.joined === true);
+  return (
+    <>
+      <div className="profile-container">
+        <div className="profile-missions-section">
+          <h3 className="mission-lists">My Missions</h3>
+          { joinedMissions.map((mission) => (
+            <MissionLists
+              key={mission.mission_id}
+              mission={mission}
+            />
+          )) }
+        </div>
+        <div className="profile-rockets-section">
+          <h3 className="rockets-list">My Rockets</h3>
+          {/* <RocketLists /> */}
+        </div>
       </div>
-      <div className="profile-rockets-section">
-        <h3 className="rockets-list">My Rockets</h3>
-        <RocketLists />
-      </div>
-    </div>
-  </>
-);
+    </>
+  );
+};
 
 export default Profile;
