e01ee9297e1d1e72e2c9de397379ae22353f2bcc
peter <peterugwu64@gmail.com>
2023-01-06 14:18:54 +0100

    Fix merge conflict

--- src/Pages/Profile.js
+++ src/Pages/Profile.js
@@ -2,32 +2,13 @@ import React from 'react';
 import { useSelector } from 'react-redux';
 
 // Components
-
 import MissionLists from '../Components/MissionLists';
-// import RocketLists from '../Components/RocketLists';
+import RocketLists from '../Components/RocketLists';
 
 // Stylesheet
 import './Profile.css';
 
 const Profile = () => {
-
-  const missions = useSelector((state) => state.Missions);
-  const joinedMissions = missions.filter((mission) => mission.joined === true);
-  return (
-    <>
-      <div className="profile-container">
-        <div className="profile-missions-section">
-          <h3 className="mission-lists">My Missions</h3>
-          { joinedMissions.map((mission) => (
-            <MissionLists
-              key={mission.mission_id}
-              mission={mission}
-            />
-          )) }
-        </div>
-        <div className="profile-rockets-section">
-          <h3 className="rockets-list">My Rockets</h3>
-          {/* <RocketLists /> */}
   const rockets = useSelector((state) => state.Rockets);
   const missions = useSelector((state) => state.Missions);
   const reservedRockets = rockets.filter((item) => item.reserved === true);
