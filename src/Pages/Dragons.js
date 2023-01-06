3a54dfd1a6d43d44275ac62a26fa99cf8b784943
umair-rao <umairmuqeem@gmail.com>
2023-01-06 16:04:34 +0500

    Implement Dragon Booking

--- src/Pages/Dragons.js
+++ src/Pages/Dragons.js
@@ -1,13 +1,24 @@
 import React from 'react';
-import { useSelector } from 'react-redux';
+import { useDispatch, useSelector } from 'react-redux';
 import DisplayDragons from '../Components/DisplayDragons';
+import { toggleReservation } from '../Redux/Dragon/Dragon';
 
 const Dragons = () => {
+  const dispatch = useDispatch();
   const dragons = useSelector((state) => state.Dragons);
+  const eventHandler = (id) => {
+    dispatch(toggleReservation(id));
+  };
 
   return (
     <div className="dragon-section">
-      {dragons.map((dragon) => <DisplayDragons key={dragon.id} dragon={dragon} />)}
+      {dragons.map((dragon) => (
+        <DisplayDragons
+          key={dragon.id}
+          dragon={dragon}
+          eventHandler={eventHandler}
+        />
+      ))}
     </div>
   );
 };
